cb2613f1e8bd362d4976dcb5a1987215c210d94e
S MOHAMMAD ASHRAF <ashrafali31033@gmail.com>
2024-12-13 10:38:11 +0530

    Create MethodsOfLinkedList.java

---
+++ MethodsOfLinkedList.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class MethodsOfLinkedList
+{
+    public static void main(String args[])
+    {
+        LinkedList<Integer> a=new LinkedList<>();
+        a.add(567);
+        a.add(789);
+        a.add(900);
+        System.out.println(a);
+        a.addFirst(89);
+        System.out.println(a);
+        a.addLast(90);
+        System.out.println(a);
+    } 
+}
